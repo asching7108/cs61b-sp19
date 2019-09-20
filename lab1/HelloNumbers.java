a763ea4922e72e71db72dac1ddb4dd570c37cae4
asching7108 <asching7108@gmail.com>
2019-09-20 14:08:49 -0400

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+	int sum = 0;
+        while (x < 10) {
+            System.out.print(sum + " ");
+            x = x + 1;
+            sum = sum + x;
+        }
+        System.out.println();
+    }
+}
