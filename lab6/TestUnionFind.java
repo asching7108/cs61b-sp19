45071861872ce3b9ed60b59a4c173fe36511d9a1
asching7108 <asching7108@gmail.com>
2019-09-26 20:11:52 -0400

    completed lab6

---
+++ lab6/TestUnionFind.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestUnionFind {
+    @Test
+    public void testUnionFind() {
+        UnionFind uf = new UnionFind(16);
+        uf.union(2, 3);
+        uf.union(1, 2);
+        uf.union(5, 7);
+        uf.union(8, 4);
+        uf.union(7, 2);
+        assertEquals(3, uf.find(2));
+        uf.union(0, 6);
+        uf.union(0, 4);
+        uf.union(0, 2);
+        assertEquals(6, uf.parent(0));
+        assertEquals(3, uf.find(0));
+        assertEquals(9, uf.sizeOf(0));
+        System.out.println(uf);
+        // Expected parentArray:
+        // 6	3	3	-9	3	7	4	3	4	-1	-1	-1	-1	-1	-1	-1
+    }
+
+    @Test
+    public void testUnionFindWithPathCompression() {
+        UnionFind uf = new UnionFind(16);
+        uf.unionWithPathCompassion(2, 3);
+        uf.unionWithPathCompassion(1, 2);
+        uf.unionWithPathCompassion(5, 7);
+        uf.unionWithPathCompassion(8, 4);
+        uf.unionWithPathCompassion(7, 2);
+        assertEquals(3, uf.find(2));
+        uf.unionWithPathCompassion(0, 6);
+        uf.unionWithPathCompassion(0, 4);
+        uf.unionWithPathCompassion(0, 2);
+        assertEquals(3, uf.parent(0));
+        assertEquals(3, uf.find(0));
+        assertEquals(9, uf.sizeOf(0));
+        System.out.println(uf);
+        // Expected parentArray:
+        // 3	3	3	-9	3	7	4	3	4	-1	-1	-1	-1	-1	-1	-1
+    }
+}
--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,58 @@
+/** Union Find (Disjoint Sets)
+ *  This program uses Weighted Quick Union (WQU) with / without
+ *  path compression implementation.
+ * @Author Hsingyi Lin
+ * date    09/26/2019
+ */
+
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    /**
+     * Array[i] is the parent item of item i. If item i is the root,
+     * Array[i] is the negative size of the set i belongs to.
+     */
+    private int[] parentArray;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        parentArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            parentArray[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0 || vertex >= parentArray.length) {
+            throw new IllegalArgumentException("Invalid input vertex");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        return -parentArray[find(v1)];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return parentArray[v1];
+    }
+
+    /* Returns the root of the set V belongs to. Path-compression is employed
+       allowing for fast search-time. */
+    public int find(int vertex) {
+        while (parent(vertex) > -1) {
+            vertex = parent(vertex);
+        }
+        return vertex;
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +61,55 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if (root1 == root2) {
+            return;
+        }
+        if (sizeOf(root1) > sizeOf(root2)) {
+            parentArray[root1] += parentArray[root2];
+            parentArray[root2] = root1;
+        }
+        else {
+            parentArray[root2] += parentArray[root1];
+            parentArray[root1] = root2;
+        }
     }
 
-    /* Returns the root of the set V belongs to. Path-compression is employed
-       allowing for fast search-time. */
-    public int find(int vertex) {
-        // TODO
-        return -1;
+    public void unionWithPathCompassion(int v1, int v2) {
+        int root1 = find(v1);
+        int root2 = find(v2);
+        int newRoot = root2;
+        int newSize = sizeOf(root2);
+        if (root1 != root2) {
+            newSize += sizeOf(root1);
+        }
+        if (sizeOf(root1) > sizeOf(root2)) {
+            newRoot = root1;
+        }
+        parentArray[root1] = newRoot;
+        parentArray[root2] = newRoot;
+        parentArray[newRoot] = -newSize;
+
+        updateParent(v1, newRoot);
+        updateParent(v2, newRoot);
+    }
+
+    private void updateParent(int v, int p) {
+        while (parent(v) > -1) {
+            int tempV = parent(v);
+            parentArray[v] = p;
+            v = tempV;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder("");
+        for (int i = 0; i < parentArray.length; i++) {
+            output.append(parentArray[i] + "\t");
+        }
+        return output.toString();
     }
 
 }
