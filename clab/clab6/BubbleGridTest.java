b088de94be70f6f60c755ecfd77c76a42a4dfcc7
asching7108 <asching7108@gmail.com>
2019-09-29 22:39:34 -0400

    completed challenge lab6

--- clab/clab6/BubbleGridTest.java
+++ clab/clab6/BubbleGridTest.java
@@ -14,6 +14,17 @@ public class BubbleGridTest {
         validate(grid, darts, expected);
     }
 
+    @Test
+    public void TestBiggerGrid() {
+        int[][] grid = new int[][] {{1, 1, 0, 0, 1},
+                                    {1, 0, 0, 0, 1},
+                                    {1, 1, 0, 1, 1},
+                                    {1, 1, 1, 0, 1}};
+        int [][] darts = new int[][] {{2, 2}, {2, 0}, {1, 4}};
+        int[] expected = new int[] {0, 4, 3};
+        validate(grid, darts, expected);
+    }
+
     private void validate(int[][] grid, int[][] darts, int[] expected) {
         BubbleGrid sol = new BubbleGrid(grid);
         assertArrayEquals(expected, sol.popBubbles(darts));
--- clab/clab6/UnionFind.java
+++ clab/clab6/UnionFind.java
@@ -1,51 +1,115 @@
+/**             Union Find (Disjoint Sets)
+ * This program uses Weighted Quick Union (WQU) with / without
+ * path compression implementation.
+ * @Author Hsingyi Lin
+ * date    09/26/2019
+ */
+
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    /**
+     * Array[i] is the parent item of item i. If item i is the root,
+     * Array[i] is the negative size of the set i belongs to.
+     */
+    private int[] parentArray;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        parentArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            parentArray[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0 || vertex >= parentArray.length) {
+            throw new IllegalArgumentException("Invalid input vertex");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        return -parentArray[find(v1)];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
-    }
-
-    /* Returns true if nodes v1 and v2 are connected. */
-    public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
-    }
-
-    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
-       elements, and a union-by-size heuristic is used. If the sizes of the sets
-       are equal, tie break by connecting v1's root to v2's root. Unioning a 
-       vertex with itself or vertices that are already connected should not 
-       change the sets but may alter the internal structure of the data. */
-    public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        return parentArray[v1];
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        while (parent(vertex) > -1) {
+            vertex = parent(vertex);
+        }
+        return vertex;
+    }
+
+    /* Returns true if nodes v1 and v2 are connected. */
+    public boolean connected(int v1, int v2) {
+        return find(v1) == find(v2);
+    }
+
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid
+       elements, and a union-by-size heuristic is used. If the sizes of the sets
+       are equal, tie break by connecting v1's root to v2's root. Unioning a
+       vertex with itself or vertices that are already connected should not
+       change the sets but may alter the internal structure of the data. */
+    public void union(int v1, int v2) {
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if (root1 == root2) {
+            return;
+        }
+        if (sizeOf(root1) > sizeOf(root2)) {
+            parentArray[root1] += parentArray[root2];
+            parentArray[root2] = root1;
+        }
+        else {
+            parentArray[root2] += parentArray[root1];
+            parentArray[root1] = root2;
+        }
+    }
+
+    public void unionWithPathCompassion(int v1, int v2) {
+        int root1 = find(v1);
+        int root2 = find(v2);
+        int newRoot = root2;
+        int newSize = sizeOf(root2);
+        if (root1 != root2) {
+            newSize += sizeOf(root1);
+        }
+        if (sizeOf(root1) > sizeOf(root2)) {
+            newRoot = root1;
+        }
+        parentArray[root1] = newRoot;
+        parentArray[root2] = newRoot;
+        parentArray[newRoot] = -newSize;
+
+        updateParent(v1, newRoot);
+        updateParent(v2, newRoot);
+    }
+
+    private void updateParent(int v, int p) {
+        while (parent(v) > -1) {
+            int tempV = parent(v);
+            parentArray[v] = p;
+            v = tempV;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder("");
+        for (int i = 0; i < parentArray.length; i++) {
+            output.append(parentArray[i] + "\t");
+        }
+        return output.toString();
     }
 
 }
