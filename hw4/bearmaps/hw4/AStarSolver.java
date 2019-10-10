8d4ec6bb78cb7ea7fd04d8f0a83ce346d3b47c79
asching7108 <asching7108@gmail.com>
2019-10-10 13:44:25 -0400

    completed hw4

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -1,22 +1,49 @@
 package bearmaps.hw4;
 
-import bearmaps.proj2ab.DoubleMapPQ;
+import bearmaps.proj2ab.ArrayHeapMinPQ;
 
 import java.util.*;
 
-public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
-    private DoubleMapPQ<Vertex> fringes;
-    private HashMap<Vertex, Double> distTo;
-    private HashMap<Vertex, Vertex> edgeTo;
-    private int numStatesExplored;
-    private SolverOutcome outcome;
-    private List<Vertex> solution;
-    private double solutionWeight;
-    private double timeSpent;
+/**
+ * {@code AStarSolver} implements A* algorithm to solve shortest path problems.
+ *
+ * @author Hsing Yi Lin
+ * date    10/10/2019
+ */
 
+public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
+    private ArrayHeapMinPQ<Vertex> fringes;  // the PQ stores vertices to be visited
+    private HashMap<Vertex, Double> distTo;  // distance from the start vertex
+    private HashMap<Vertex, Vertex> edgeTo;  // edge leading to the vertices
+    private int numStatesExplored;           // number of vertices visited
+    private SolverOutcome outcome;           // problem solve outcome
+    private List<Vertex> solution;           // vertices of the shortest path in order
+    private double solutionWeight;           // total weight of the shortest path
+    private double timeSpent;                // total time spent finding the shortest path
+
+    /**
+     * Finds the shortest path from {@code start} to {@code end} of the graph {@code input}
+     * using a* algorithm. Initialize variables and stores the outcome and solution. If the
+     * given timeout {@code timeout} is exceeded, it stops running.
+     *
+     * The A* algorithm:
+     *     Adds the start vertex in the PQ
+     *     If PQ is not empty:
+     *         remove the smallest vertex p from PQ
+     *         if p equals goal:
+     *             the shortest path is found
+     *         else:
+     *         relax all edges outgoing from p
+     *
+     * @param input the graph
+     * @param start the start vertex
+     * @param end the goal vertex
+     * @param timeout the timeout value
+     */
     public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout) {
 
-        fringes = new DoubleMapPQ<>();
+        // Initialize all variables
+        fringes = new ArrayHeapMinPQ<>();
         distTo = new HashMap<>();
         edgeTo = new HashMap<>();
         numStatesExplored = 0;
@@ -29,23 +56,43 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         distTo.put(start, 0.0);
 
         // Repeats until the PQ is empty, PQ.getSmallest() is the goal, or timeout is exceeded
+        long startTime = System.currentTimeMillis();  // timing start
         while (fringes.size() != 0) {
             Vertex p = fringes.removeSmallest();
+            timeSpent = (System.currentTimeMillis() - startTime)/1000.0;
             numStatesExplored++;
+            // If goal is found, stops running and updates the solution
             if (p.equals(end)) {
                 outcome = SolverOutcome.SOLVED;
                 updateSolution(start, end);
                 solutionWeight = distTo.get(end);
                 return;
             }
-            relax(input, end, input.neighbors(p));
+            // If timeout is exceeded, stops running
+            if (timeSpent > timeout) {
+                outcome = SolverOutcome.TIMEOUT;
+                return;
+            }
+            relax(input, end, p);
         }
+        // Goal is not found
+        timeSpent = (System.currentTimeMillis() - startTime)/1000.0;
         outcome = SolverOutcome.UNSOLVABLE;
     }
 
-    private void relax(AStarGraph<Vertex> input, Vertex end, List<WeightedEdge<Vertex>> neighbors) {
+    /**
+     * For all neighbor vertices of {@code p}, adds or updates the priority of the neighbor
+     * vertex q to the {@code fringes} if the distance from start to p + the weight of edge
+     * p-q is less than the previous distance from start to q or if q is never considered to
+     * be relaxed.
+     *
+     * @param input the graph
+     * @param end the goal vertex
+     * @param p the current vertex of which the edges out to be relaxed
+     */
+    private void relax(AStarGraph<Vertex> input, Vertex end, Vertex p) {
+        List<WeightedEdge<Vertex>> neighbors = input.neighbors(p);
         for (WeightedEdge<Vertex> e : neighbors) {
-            Vertex p = e.from();
             Vertex q = e.to();
             double w = e.weight();
             if (!distTo.containsKey(q) || distTo.get(p) + w < distTo.get(q)) {
@@ -61,6 +108,13 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         }
     }
 
+    /**
+     * Stores the vertices of the solution to shortest path in order the the list of vertices
+     * {@code solution}.
+     *
+     * @param start the start vertex
+     * @param end the goal vertex
+     */
     private void updateSolution(Vertex start, Vertex end) {
         solution.add(end);
         Vertex curr = end;
@@ -83,5 +137,5 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     public int numStatesExplored() { return numStatesExplored; }
 
     @Override
-    public double explorationTime() { return 0; }
+    public double explorationTime() { return timeSpent; }
 }
---
+++ hw4/bearmaps/proj2ab/ArrayHeapMinPQ.java
@@ -0,0 +1,269 @@
+package bearmaps.proj2ab;
+
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+/**
+ * The {@code ArrayHeapMinPQ} class implements the ExtrinsicMinPQ interface,
+ * representing a priority queue of generic keys with extrinsic priority.
+ * Uses a one-based array to simplify parent and child calculations.
+ *
+ * @author Hsingyi Lin
+ * date    10/03/2019
+ */
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private static final int INIT_SIZE = 100;
+    private static final double INIT__MIN_LOADFACTOR = 0.25;
+    private static final double INIT_MAX_LOADFACTOR = 0.75;
+
+    private PriorityNode[] heap;        // store items at indices 1 to n
+    private HashMap<T, Integer> items;  // store items with indices in heap
+    private double loadFactorMin;
+    private double loadFactorMax;
+
+    /**
+     * Initializes an empty priority queue.
+     */
+    public ArrayHeapMinPQ() {
+        heap = new ArrayHeapMinPQ.PriorityNode[INIT_SIZE];
+        items = new HashMap<>();
+        loadFactorMin = INIT__MIN_LOADFACTOR;
+        loadFactorMax = INIT_MAX_LOADFACTOR;
+    }
+
+    /**
+     * {@code PriorityNode} class stores the item and priority, with comparability
+     * among priorities.
+     */
+    private class PriorityNode implements Comparable<PriorityNode> {
+
+        T item;
+        double priority;
+
+        PriorityNode(T i, double p) {
+            item = i;
+            priority = p;
+        }
+
+        T getItem() { return item; }
+
+        double getPriority() { return priority; }
+
+        void setPriority(double p) { priority = p; }
+
+        @Override
+        public int compareTo(PriorityNode o) {
+            return Double.compare(this.priority, o.priority);
+        }
+    }
+
+    /**
+     * Adds an item of type T with the given priority.
+     *
+     * @param item the item
+     * @param priority the extrinsic priority
+     * @throws IllegalArgumentException if {@code item} already exists
+     */
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException("argument to add() already exists in PQ");
+        }
+        if (size() / (double) heap.length > loadFactorMax) {
+            resize(heap.length * 2);
+        }
+        int pos = size() + 1;
+        heap[pos] = new PriorityNode(item, priority);
+        items.put(item, pos);
+        swim(pos);
+    }
+
+    /**
+     * Recursively swaps the item at {@code x} with its parent item if {@code x} has
+     * lower priority than its parent, and so forth.
+     *
+     * @param x the index of the item at the array
+     */
+    private void swim(int x) {
+        if (heap[x].compareTo(heap[parent(x)]) < 0) {
+            swap(x, parent(x));
+            swim(parent(x));
+        }
+    }
+
+    /**
+     * Returns the index of the parent item of the item at index {@code x}. If the
+     * item at index {@code x} is the root, return its own index.
+     *
+     * @param x the index of the item at the array
+     * @return the index of the parent item
+     */
+    private int parent(int x) {
+        if (x == 1) {
+            return 1;
+        }
+        return x / 2;
+    }
+
+    /**
+     * Swaps the item at {@code x} with the item at {@code y}.
+     *
+     * @param x the index of the item at the array
+     * @param y the index of the item at the array
+     */
+    private void swap(int x, int y) {
+        if (x == y) {
+            return;
+        }
+        PriorityNode temp = heap[x];
+        heap[x] = heap[y];
+        heap[y] = temp;
+        items.put(heap[x].getItem(), x);
+        items.put(heap[y].getItem(), y);
+    }
+
+    /**
+     * Resize the heap to the given new size.
+     *
+     * @param newSize the new size
+     */
+    private void resize(int newSize) {
+        PriorityNode[] newHeap = new ArrayHeapMinPQ.PriorityNode[newSize];
+        for (int i = 1; i <= size(); i++) {
+            newHeap[i] = heap[i];
+        }
+        heap = newHeap;
+    }
+
+    /**
+     * Returns true if the PQ contains the given {@code item}.
+     *
+     * @param item the item
+     * @return {@code true} if the PQ contains the given {@code item}, {@code false}
+     * otherwise.
+     */
+    @Override
+    public boolean contains(T item) {
+        if (size() == 0) {
+            return false;
+        }
+        return items.containsKey(item);
+    }
+
+    /**
+     * Returns the item with smallest priority in the PQ.
+     *
+     * @return the item with smallest priority
+     * @throws NoSuchElementException if PQ is empty
+     */
+    @Override
+    public T getSmallest() {
+        if (size() == 0) {
+            throw new NoSuchElementException("PQ is empty");
+        }
+        return heap[1].getItem();
+    }
+
+    /**
+     * Removes and returns the item with smallest priority in the PQ.
+     *
+     * @return the item with smallest priority
+     * @throws NoSuchElementException if PQ is empty
+     */
+    @Override
+    public T removeSmallest() {
+        if (size() == 0) {
+            throw new NoSuchElementException("PQ is empty");
+        }
+        if (size() / (double) heap.length < loadFactorMin) {
+            resize(heap.length / 2);
+        }
+        T smallest = heap[1].getItem();
+        heap[1] = heap[size()];
+        heap[size()] = null;
+        items.remove(smallest);
+        if (size() > 0) {
+            sink(1);
+        }
+        return smallest;
+    }
+
+    /**
+     * Recursively swaps the item at {@code x} with its smallest child item if
+     * {@code x} has higher priority than its child, and so forth.
+     *
+     * @param x the index of the item at the array
+     */
+    private void sink(int x) {
+        if (heap[x].compareTo(heap[child(x)]) > 0) {
+            int smallestChild = child(x);
+            swap(x, smallestChild);
+            sink(smallestChild);
+        }
+    }
+
+    /**
+     * Returns the index of the smallest child item of the item at index {@code x}.
+     * If the item at index {@code x} has no child, return its own index.
+     *
+     * @param x the index of the item at the array
+     * @return the index of the smallest child item
+     */
+    private int child(int x) {
+        if (size() < x * 2) {
+            return x;
+        }
+        if (size() == x * 2 || heap[x * 2].compareTo(heap[x * 2 + 1]) <= 0) {
+            return x * 2;
+        }
+        return x * 2 + 1;
+    }
+
+    /**
+     * Returns the number of items.
+     *
+     * @return the number of items
+     */
+    @Override
+    public int size() {
+        return items.size();
+    }
+
+    /**
+     * Sets the priority of the given item to the given value.
+     *
+     * @param item the item
+     * @param priority the priority
+     * @throws NoSuchElementException if {@code item} does not exist
+     */
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new IllegalArgumentException("argument to changePriority() does not exist");
+        }
+        int i = items.get(item);
+        double oldPriority = heap[i].getPriority();
+        heap[i].setPriority(priority);
+        if (priority > oldPriority) {
+            sink(i);
+        }
+        else {
+            swim(i);
+        }
+    }
+
+    public T[] getItems() {
+        T[] i = (T []) new Object[size()];
+        return items.keySet().toArray(i);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 1; i <= size(); i++) {
+            sb.append(heap[i].getItem() + " ");
+        }
+        return sb.toString();
+    }
+}
