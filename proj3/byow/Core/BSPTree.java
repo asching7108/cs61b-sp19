edfbbf352fe7f8e9e775653c87a8b5da2831264c
asching7108 <asching7108@gmail.com>
2019-10-25 00:12:42 -0400

    completed world generation

--- proj3/byow/Core/BSPTree.java
+++ proj3/byow/Core/BSPTree.java
@@ -3,7 +3,7 @@ package byow.Core;
 import java.util.*;
 
 public class BSPTree {
-    private static final int MIN_SIZE = 4;
+    private static final int MIN_SIZE = 6;
     private static final int ROOM_SIZE_MIN = 4;
 
     Random r;
@@ -13,7 +13,7 @@ public class BSPTree {
 
     BSPTree(int w, int h, int leafNum, Random r) {
         this.r = r;
-        root = new Leaf(0, 0, w, h, -1);
+        root = new Leaf(0, 0, w, h);
         rooms = new LinkedList<>();
         hallways = new LinkedList<>();
         ArrayDeque<Leaf> pq = new ArrayDeque<>();
@@ -28,6 +28,7 @@ public class BSPTree {
             }
         }
         createRooms(root);
+        createHallway(root);
     }
 
     class Leaf {
@@ -40,18 +41,21 @@ public class BSPTree {
         Leaf left;
         Leaf right;
 
-        Leaf(int x, int y, int w, int h, int d) {
+        Leaf(int x, int y, int w, int h) {
             this.x = x;
             this.y=  y;
             this.w = w;
             this.h = h;
-            direction = d;
+            direction = -1;
             room = null;
             left = null;
             right = null;
         }
 
-        void createRoom() {
+        Room createRoom() {
+            if (RandomUtils.uniform(r) > 0.8) {
+                return null;
+            }
             int min_width = Math.max(ROOM_SIZE_MIN, w / 2 + 2);
             int min_height = Math.max(ROOM_SIZE_MIN, h / 2 + 2);
             int width = RandomUtils.uniform(r, w - min_width + 1) + min_width;
@@ -65,6 +69,7 @@ public class BSPTree {
                 yOffset = y + RandomUtils.uniform(r, h - height);
             }
             this.room = new Room(xOffset, yOffset, width, height);
+            return this.room;
         }
     }
 
@@ -84,54 +89,45 @@ public class BSPTree {
             direction = (direction + 1) % 2;
             length = direction == 0 ? leaf.w : leaf.h;
         }
+        leaf.direction = direction;
+        System.out.println(direction);
         // splits into two leaves with random size no less than the MIN_SIZE
         int split = RandomUtils.uniform(r, length - MIN_SIZE * 2 + 1) + MIN_SIZE;
         if (direction == 0) {
-            leaf.left = new Leaf(leaf.x, leaf.y, split, leaf.h, direction);
-            leaf.right = new Leaf(leaf.x + split - 1, leaf.y, leaf.w - split, leaf.h, direction);
+            leaf.left = new Leaf(leaf.x, leaf.y, split, leaf.h);
+            leaf.right = new Leaf(leaf.x + split - 1, leaf.y, leaf.w - split, leaf.h);
         }
         else {
-            leaf.left = new Leaf(leaf.x, leaf.y, leaf.w, split, direction);
-            leaf.right = new Leaf(leaf.x, leaf.y + split - 1, leaf.w, leaf.h - split, direction);
+            leaf.left = new Leaf(leaf.x, leaf.y, leaf.w, split);
+            leaf.right = new Leaf(leaf.x, leaf.y + split - 1, leaf.w, leaf.h - split);
         }
         return true;
     }
 
-    private Room createRooms(Leaf leaf) {
+    private void createRooms(Leaf leaf) {
         if (leaf.left == null && leaf.right == null) {
-            leaf.createRoom();
-            rooms.add(leaf.room);
-            return leaf.room;
+            if (leaf.createRoom() != null) {
+                rooms.add(leaf.room);
+            }
         }
-        Room leftRoom = createRooms(leaf.left);
-        Room rightRoom = createRooms(leaf.right);
-        createHallway(leftRoom, rightRoom);
-        return RandomUtils.uniform(r) < 0.5 ? leftRoom : rightRoom;
     }
 
-    private void createHallway(Room r1, Room r2) {
-        boolean xOverlap = r1.xOffset() + r1.width() > r2.xOffset() + 2
-                && r2.xOffset() + r2.width() > r1.xOffset() + 2;
-        boolean yOverlap = r1.yOffset() + r1.height() > r2.yOffset() + 2
-                && r2.yOffset() + r2.height() > r1.yOffset() + 2;
-        if (xOverlap) {
-            int overlapStart = Math.max(r1.xOffset(), r2.xOffset());
-            int overlapEnd = Math.min(r1.xOffset() + r1.width() - 1, r2.xOffset() + r2.width() - 1);
-            int x = RandomUtils.uniform(r, overlapEnd - overlapStart - 1) + overlapStart + 1;
-            int y = Math.min(r1.yOffset() + r1.height(), r2.yOffset() + r2.height()) - 2;
-            int length = Math.max(r1.yOffset(), r2.yOffset()) - y + 2;
-            Room hallway = new Room(x - 1, y, 3, length);
-            hallways.add(hallway);
+    private void createHallway(Leaf leaf) {
+        if (leaf.left == null && leaf.right == null) {
+            return;
         }
-        if (yOverlap) {
-            int overlapStart = Math.max(r1.yOffset(), r2.yOffset());
-            int overlapEnd = Math.min(r1.yOffset() + r1.height() - 1, r2.yOffset() + r2.height() - 1);
-            int y = RandomUtils.uniform(r, overlapEnd - overlapStart - 1) + overlapStart + 1;
-            int x = Math.min(r1.xOffset() + r1.width(), r2.xOffset() + r2.width()) - 2;
-            int length = Math.max(r1.xOffset(), r2.xOffset()) - x + 2;
-            Room hallway = new Room(x, y - 1, length, 3);
-            hallways.add(hallway);
+        if (leaf.direction == 0) {
+            int start = leaf.left.x + leaf.left.w / 2 - 1;
+            int end = leaf.right.x + leaf.right.w / 2 + 1;
+            hallways().add(new Room(start, leaf.y + leaf.h / 2 - 1, end - start + 1, 3));
         }
+        else {
+            int start = leaf.left.y + leaf.left.h / 2 - 1;
+            int end = leaf.right.y + leaf.right.h / 2 + 1;
+            hallways().add(new Room(leaf.x + leaf.w / 2 - 1, start, 3, end - start + 1));
+        }
+        createHallway(leaf.left);
+        createHallway(leaf.right);
     }
 
     public List<Room> rooms() { return rooms; }
--- proj3/byow/Core/World.java
+++ proj3/byow/Core/World.java
@@ -3,36 +3,26 @@ package byow.Core;
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
-import byow.lab6.UnionFind;
 
 import java.util.*;
 
 public class World {
-    private static final int WORLD_WIDTH = 64;
-    private static final int WORLD_HEIGHT = 32;
-    private static final int ROOM_NUM_MIN = 11;
-    private static final int ROOM_NUM_RANGE = 6;
-    private static final int ROOM_SIZE_MIN = 4;
-    private static final int ROOM_SIZE_RANGE = 6;
-    private static final int HALLWAY_NUM_MIN = 11;
-    private static final int HALLWAY_NUM_RANGE = 6;
-    private static final int HALLWAY_SIZE_MIN = 3;
-    private static final int HALLWAY_SIZE_RANGE = 20;
+    private static final int WORLD_WIDTH = 60;
+    private static final int WORLD_HEIGHT = 40;
 
     private Random random;
     private TETile[][] world;
-    private int[][] worldOfRooms;
-    private UnionFind<Room> rooms;
+    private BSPTree bsp;
 
-    public World(long seed) {
+
+    World(int seed) {
         random = new Random(seed);
         TERenderer ter = new TERenderer();
         ter.initialize(WORLD_WIDTH, WORLD_HEIGHT);
         world = new TETile[WORLD_WIDTH][WORLD_HEIGHT];
-        worldOfRooms = new int[WORLD_WIDTH][WORLD_HEIGHT];
+        bsp = new BSPTree(WORLD_WIDTH, WORLD_HEIGHT, 30, random);
         initializeWorld();
-        rooms = generateRooms(random, world);
-        generateHallways(world, random, rooms);
+        generateWorld();
         ter.renderFrame(world);
     }
 
@@ -49,163 +39,53 @@ public class World {
         }
     }
 
-    class Room {
-        int width;
-        int height;
-        int xOffset;
-        int yOffset;
-
-        Room(int w, int h, int x, int y) {
-            width = w;
-            height = h;
-            xOffset = x;
-            yOffset = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (this.getClass() != o.getClass()) {
-                return false;
-            }
-            Room other = (Room) o;
-            return this.width == other.width && this.height == other.height
-                    && this.xOffset == other.xOffset && this.yOffset == other.yOffset;
-        }
-
-        @Override
-        public int hashCode() {
-            return super.hashCode();
-        }
-
-        @Override
-        public String toString() {
-            return ("start from " + "(" + xOffset + ", " + yOffset + ") "
-                + "width=" + width + " height=" + height);
-        }
-    }
 
     private void initializeWorld() {
         for (int x = 0; x < world.length; x++) {
             for (int y = 0; y < world[0].length; y++) {
                 world[x][y] = Tileset.NOTHING;
-                worldOfRooms[x][y] = -1;
             }
         }
     }
 
-    private UnionFind<Room> generateRooms(Random r, TETile[][] world) {
-        UnionFind<Room> rooms = new UnionFind<>(10);
-        int num = RandomUtils.uniform(r, ROOM_NUM_RANGE) + ROOM_NUM_MIN;
-        while (num > 0) {
-            int width = RandomUtils.uniform(r, ROOM_SIZE_RANGE) + ROOM_SIZE_MIN;
-            int height = RandomUtils.uniform(r, ROOM_SIZE_RANGE) + ROOM_SIZE_MIN;
-            int xOffset = RandomUtils.uniform(r, WORLD_WIDTH - width);
-            int yOffset = RandomUtils.uniform(r, WORLD_HEIGHT - height);
-            Room room = new Room(width, height, xOffset, yOffset);
-            if (!isOverlap(world, room)) {
-                int roomIdx = rooms.add(room);
-                System.out.println(roomIdx);
-                addRoom(world, room, roomIdx);
-                num--;
-            }
+    private void generateWorld() {
+        LinkedList<Room> rooms = (LinkedList) bsp.rooms();
+        for (Room r : rooms) {
+            System.out.println(r);
+            addRoom(world, r);
         }
-        return rooms;
-    }
-
-    private void generateHallways(TETile[][] world, Random r, UnionFind rooms) {
-        while (rooms.maxConnected() < 25) {
-            int width = RandomUtils.uniform(r, HALLWAY_SIZE_RANGE) + HALLWAY_SIZE_MIN;
-            int height = HALLWAY_SIZE_MIN;
-            if (RandomUtils.uniform(r) < 0.5) {
-                int temp = width;
-                width = height;
-                height = temp;
-            }
-            int xOffset = RandomUtils.uniform(r, WORLD_WIDTH - width);
-            int yOffset = RandomUtils.uniform(r, WORLD_HEIGHT - height);
-            Room room = new Room(width, height, xOffset, yOffset);
-
-            int roomIdx = rooms.add(room);
-            System.out.println(roomIdx);
-            HashSet<Integer> overlapRoom = (HashSet) overlapRooms(room);
-            if (overlapRoom.size() > 0) {
-                addRoom(world, room, roomIdx);
-                for (int idx : overlapRoom) {
-                    rooms.union(roomIdx, idx);
-                }
-            }
-        }
-        for (int i = 0; i < rooms.size(); i++) {
-            if (rooms.connectedItems(i) < 25) {
-                removeRoom((Room) rooms.getItem(i));
-                System.out.println(i);
-            }
+        LinkedList<Room> hallways = (LinkedList) bsp.hallways();
+        for (Room r : hallways) {
+            System.out.println(r);
+            addRoom(world, r);
         }
     }
 
-    private void removeRoom(Room room) {
-        for (int y = 0; y < room.height; y++) {
-            for (int x = 0; x < room.width; x++) {
-                world[room.xOffset + x][room.yOffset + y] = Tileset.NOTHING;
-                worldOfRooms[room.xOffset + x][room.yOffset + y] = -1;
-            }
+    private void addRoom(TETile[][] world, Room room) {
+        for (int y = 0; y < room.height(); y++) {
+            addRowOfRoom(world, room, y);
         }
     }
 
-    private boolean isOverlap(TETile[][] world, Room room) {
-        for (int y = 0; y < room.height; y++) {
-            for (int x = 0; x < room.width; x++) {
-                if (world[room.xOffset + x][room.yOffset + y].equals(Tileset.FLOOR)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private void addRoom(TETile[][] world, Room room, int roomIdx) {
-        for (int y = 0; y < room.height; y++) {
-            addRowOfRoom(world, room, roomIdx, y);
-        }
-    }
-
-    private void addRowOfRoom(TETile[][] world, Room room, int roomIdx, int row) {
-        int y = room.yOffset + row;
-        if (row == 0 || row == room.height - 1) {
-            for (int x = 0; x < room.width; x++) {
-                addTile(world, new Position(room.xOffset + x, y), Tileset.WALL, roomIdx);
+    private void addRowOfRoom(TETile[][] world, Room room, int row) {
+        int y = room.yOffset() + row;
+        if (row == 0 || row == room.height() - 1) {
+            for (int x = 0; x < room.width(); x++) {
+                addTile(world, new Position(room.xOffset() + x, y), Tileset.WALL);
             }
             return;
         }
-        addTile(world, new Position(room.xOffset, y), Tileset.WALL, roomIdx);
-        for (int x = 1; x < room.width - 1; x++) {
-            addTile(world, new Position(room.xOffset + x, y), Tileset.FLOOR, roomIdx);
+        addTile(world, new Position(room.xOffset(), y), Tileset.WALL);
+        for (int x = 1; x < room.width() - 1; x++) {
+            addTile(world, new Position(room.xOffset() + x, y), Tileset.FLOOR);
         }
-        addTile(world, new Position(room.xOffset + room.width - 1, y), Tileset.WALL, roomIdx);
+        addTile(world, new Position(room.xOffset() + room.width() - 1, y), Tileset.WALL);
     }
 
-    private void addTile(TETile[][] world, Position p, TETile t, int roomIdx) {
-        if (world[p.x][p.y].equals(Tileset.NOTHING)) {
-            worldOfRooms[p.x][p.y] = roomIdx;
-        }
+    private void addTile(TETile[][] world, Position p, TETile t) {
         if (!t.equals(Tileset.WALL) || !world[p.x][p.y].equals(Tileset.FLOOR)) {
             world[p.x][p.y] = t;
         }
-    }
-
-    private Collection<Integer> overlapRooms(Room room) {
-        HashSet<Integer> res = new HashSet<>();
-        for (int x = 0; x < room.width; x++) {
-            for (int y = 0; y < room.height; y++) {
-                if (world[room.xOffset + x][room.yOffset + y] == Tileset.FLOOR) {
-                    res.add(worldOfRooms[room.xOffset + x][room.yOffset + y]);
-                }
-            }
-        }
-        return res;
     }
 
     public static void main(String[] args) {
